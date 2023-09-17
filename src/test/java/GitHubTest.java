0cc2c1b921caeef529674c286d7151fc3b5086b8
Artem Kozlov <artem.art16@mail.ru>
2023-09-17 21:31:21 +0300

    Answer

--- src/test/java/GitHubTest.java
+++ src/test/java/GitHubTest.java
@@ -1,8 +1,6 @@
-import com.codeborne.selenide.CollectionCondition;
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-
 import static com.codeborne.selenide.CollectionCondition.itemWithText;
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.*;
@@ -21,10 +19,36 @@ public class GitHubTest {
         $$("[type='button']").findBy(text("Show 2 more pages")).click();
         $$("span.Truncate").shouldHave(itemWithText("SoftAssertions"));
         $$("span.Truncate").findBy(text("SoftAssertions")).click();
-        $$("h4").shouldHave(itemWithText("3. Using JUnit5 extend test class:"));
+        $$("div.highlight-source-java").shouldHave(itemWithText("@ExtendWith({SoftAssertsExtension.class})\n" +
+                "class Tests {\n" +
+                "  @Test\n" +
+                "  void test() {\n" +
+                "    Configuration.assertionMode = SOFT;\n" +
+                "    open(\"page.html\");\n" +
+                "\n" +
+                "    $(\"#first\").should(visible).click();\n" +
+                "    $(\"#second\").should(visible).click();\n" +
+                "  }\n" +
+                "}"));
 
 
-        //sleep(5000);
+        /*
+        Ответ на вопрос:
+        Разница есть:
+        $("h1 div") - вернет первый попавшийся элемент div, который находится под элементом h1
+        не находились.
+        $("h1").$("div"); - найдет первый попавшийся элемент h1 и будет искать у него дочерний
+        елемент div
+        Для данного случая код $("h1").$("div") упадет в ошибку:
+        <h1>
+             <a>Text1</a>
+        </h1>
+        <h1>
+             <div>Text2</div>
+        </h1>
+        */
+
+
     }
 
 }
